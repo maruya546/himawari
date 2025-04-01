24188065d74e23ab816fe29d1c51a9bf01329b7f
まるや <himawari8663@outlook.jp>
2025-04-01 16:59:04 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -14,6 +14,7 @@
   });
 
   close.addEventListener('click', () => {
+    window.scrollTo(0, 0);
     modal.classList.add('hidden');
     mask.classList.add('hidden');
   });
@@ -124,7 +125,7 @@
     { code: 'A6 6111', service: '通所型独自サービス処遇改善加算Ⅲ', value: '所定単位数の<br><span class="a2">80 / 1000</span>', unit: '１月' },
     { code: 'A6 5612', service: '通所型独自送迎減算', value: '&minus;４７', unit: '片道' }
   ];
-  
+
   const tableData2 = [
     { type: 'basic', colspan: 4, label: '基本報酬' },
     { code: '78 1441', service: '地域通所介護５１', value: '753', unit: '１回' },
@@ -138,31 +139,31 @@
     { code: '78 6104', service: '地域通所介護処遇改善加算Ⅲ', value: '所定単位数の<br><span>80 / 1000</span>', unit: '１月' },
     { code: '78 5612', service: '地域通所介護送迎減算', value: '&minus;４７', unit: '片道' }
   ];
-  
+
   const createTable = (tableData, headingId) => {
     // テーブル作成
     const table = document.createElement('table');
-  
+
     // ヘッダー行を作成
     const thead = document.createElement('thead');
     const headerRow = document.createElement('tr');
     const headers = ['サービスコード', '基本報酬・加算名', '単位数', '算定回数'];
-  
+
     headers.forEach(text => {
       const th = document.createElement('th');
       th.textContent = text;
       headerRow.appendChild(th);
     });
-  
+
     thead.appendChild(headerRow);
     table.appendChild(thead);
-  
+
     // ボディ行を作成
     const tbody = document.createElement('tbody');
-  
+
     tableData.forEach(rowData => {
       const tr = document.createElement('tr');
-  
+
       if (rowData.type) {
         const td = document.createElement('td');
         td.colSpan = rowData.colspan;
@@ -173,30 +174,30 @@
         const td1 = document.createElement('td');
         td1.innerHTML = rowData.code;
         tr.appendChild(td1);
-  
+
         const td2 = document.createElement('td');
         td2.innerHTML = rowData.service;
         tr.appendChild(td2);
-  
+
         const td3 = document.createElement('td');
         td3.innerHTML = rowData.value;
         tr.appendChild(td3);
-  
+
         const td4 = document.createElement('td');
         td4.innerHTML = rowData.unit;
         tr.appendChild(td4);
       }
-  
+
       tbody.appendChild(tr);
     });
-  
+
     table.appendChild(tbody);
-  
+
     // 任意の場所にテーブルを追加
     const heading = document.getElementById(headingId);
     heading.insertAdjacentElement('afterend', table);
   };
-  
+
   // テーブルを作成して追加
   createTable(tableData1, 'support');  // 見出し1の下に追加
   createTable(tableData2, 'remuneration');  // 見出し2の下に追加
